efb5733a329cc4d9e1a6e89166cadeaed67c64fd
allainserv <serallain@gmail.com>
2025-02-10 13:40:09 +0800

    first commit

---
+++ app.vue
@@ -0,0 +1,8 @@
+import { NuxtLayout, NuxtPage } from './.nuxt/components';
+<template>
+    <div>
+        <NuxtLayout>
+            <NuxtPage />
+        </NuxtLayout>
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="p-5 h-screen bg-white flex items-center justify-center">
+        <Button label="Click" />
+    </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
